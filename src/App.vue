3c108792c90a98eaaed81009c50276b47c63a9d3
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-06-21 21:11:53 +0630

    Making Some Global Components to Local Components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
+    <TheHeader />
+    <BadgeList />
+    <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    />
   </div>
 </template>
 
 <script>
+import TheHeader from "@/components/TheHeader.vue";
+import UserInfo from "@/components/UserInfo.vue";
+import BadgeList from "@/components/BadgeList.vue";
+
 export default {
+  components: {
+    TheHeader,
+    UserInfo,
+    BadgeList
+  },
   data() {
     return {
       activeUser: {
