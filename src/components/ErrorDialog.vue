a5f900fb8891c06f859e6e8f31bec112ff608cc2
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-06-22 21:56:18 +0630

    Practicing

---
+++ src/components/ErrorDialog.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+export default {}
+</script>
+
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<style scoped>
+  dialog {
+    margin: 0;
+    position: fixed;
+    top: 20vh;
+    left: 30%;
+    width: 40%;
+    background-color: white;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
+  }
+</style>
--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,15 +1,38 @@
-<script lang="ts">
-import {defineComponent} from 'vue'
-
-export default defineComponent({
-  name: "ManageGoals"
-})
+<script>
+  import ErrorDialog from "@/components/ErrorDialog.vue";
+  export default {
+    components: {
+      ErrorDialog,
+    },
+    data() {
+      return {
+        errorIsInvalid: false,
+      }
+    },
+    methods: {
+      addGoal() {
+        const enteredGoal = this.$refs.goal.value;
+        if (enteredGoal === '') {
+          this.errorIsInvalid = true;
+        }
+      },
+      confirmError() {
+        this.errorIsInvalid = false;
+      }
+    }
+  }
 </script>
 
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text"/>
+    <input type="text" ref="goal"/>
+    <button @click="addGoal">Submit</button>
+    <error-dialog v-if="errorIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least a few characters...</p>
+      <button @click="confirmError">OK</button>
+    </error-dialog>
   </div>
 </template>
 
